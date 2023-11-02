a61c66cda8a6c98699aae0a23a0d574f1a0199dd
Bartlomiej Mroczkowski <bartlomiej.mroczkowski77@gmail.com>
2023-11-02 04:42:02 +0100

    Finished & polished rest api functionalities

--- src/test/java/com/azubu/restapi/RestApiApplicationTests.java
+++ src/test/java/com/azubu/restapi/RestApiApplicationTests.java
@@ -1,10 +1,5 @@
 package com.azubu.restapi;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 class RestApiApplicationTests {
 
 
